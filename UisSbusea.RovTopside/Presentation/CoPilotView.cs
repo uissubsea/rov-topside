49e690d458bc7f57657779fa8ce8437efc807583
MortenW <Morten_w92@hotmail.com>
2014-02-12 14:42:49 +0100

    CoPilotView with 2 diff. camera views

--- UisSbusea.RovTopside/Presentation/CoPilotView.cs
+++ UisSbusea.RovTopside/Presentation/CoPilotView.cs
@@ -15,10 +15,16 @@ namespace UisSubsea.RovTopside.Presentation
     {
         private Camera camera1, camera2;
         private Size hd;
+        private Size smallCamView;
+        private Boolean fullScreen;
+
         public CoPilotView()
         {
             InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+
             hd = new Size(1280, 720);
+            smallCamView = new Size(640, 360);
             this.FormClosing += Form1_FormClosing;
         }
 
@@ -36,6 +42,69 @@ namespace UisSubsea.RovTopside.Presentation
                 camera1.Dispose();
                 camera2.Dispose();
             }
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Escape)
+            {
+                this.Close();
+                return true;
+            }
+            else if (keyData == Keys.F11)
+            {
+                if (!fullScreen)
+                {
+                    fullScreen = true;
+                    this.WindowState = FormWindowState.Normal;
+                    this.FormBorderStyle = FormBorderStyle.None;
+                    this.WindowState = FormWindowState.Maximized;
+                }
+                else
+                {
+                    fullScreen = false;
+                    this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
+                    this.WindowState = FormWindowState.Normal;
+                }
+            }
+            if (keyData == Keys.F1)
+            {
+                if (pictureBox2.Visible == false)
+                    pictureBox2.Visible = true;
+                if (camera1.Canvas==pictureBox1)
+                {
+                    camera1.Canvas = pictureBox2;
+                    camera2.Canvas = pictureBox1;
+                }
+                else  if(camera1.Canvas==pictureBox2)
+                {
+                    camera1.Canvas = pictureBox1;
+                    camera2.Canvas = pictureBox2;
+                }
+        
+            }
+               
+            if (keyData == Keys.F2)
+            {
+                    if(camera2.Canvas == pictureBox1)
+                    {
+                        camera2.Canvas = pictureBox2;
+                        camera1.Canvas = pictureBox1;
+                        pictureBox2.Visible = false;
+                    }
+                    else
+                    {
+                        camera1.Canvas = pictureBox2;
+                        camera2.Canvas = pictureBox1;
+                        pictureBox2.Visible = false;
+                    } 
+            }        
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void aGauge1_ValueInRangeChanged(object sender, ValueInRangeChangedEventArgs e)
+        {
+
         }
     }
 }
