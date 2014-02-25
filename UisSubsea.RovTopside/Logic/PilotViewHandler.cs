910a271687fd48d39d3c08e8f158091661495a74
Stian Sandve <stian@sandve.org>
2014-02-25 15:58:16 +0100

    Use one handler for each view

---
+++ UisSubsea.RovTopside/Logic/PilotViewHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UisSubsea.RovTopside.Logic
+{
+    public interface PilotViewHandler
+    {
+        void SetHeading(int heading);
+        void SetFrontCameraAngle(int angle);
+        void SetRearCameraAngle(int angle);
+        void SetDepth(double depth);
+        void SetLaserDistanceMeasured(double distance);
+    }
+}
