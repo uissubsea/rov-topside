62f548e10318fdb771ec59a5a6c682934050b06a
Stian Sandve <stian@sandve.org>
2014-02-26 15:53:32 +0100

    Added view interface

---
+++ UisSubsea.RovTopside/Logic/IView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UisSubsea.RovTopside.Logic
+{
+    public interface IView
+    {
+        ICamera GetCamera();
+        void SetCamera(ICamera camera);
+    }
+}
