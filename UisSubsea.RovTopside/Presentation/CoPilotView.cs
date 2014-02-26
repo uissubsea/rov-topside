a3aed264528949449698d49c0051edba438db147
Stian Sandve <stian@sandve.org>
2014-02-26 19:04:53 +0100

    Merge conflict fix

--- UisSubsea.RovTopside/Presentation/CoPilotView.cs
+++ UisSubsea.RovTopside/Presentation/CoPilotView.cs
@@ -14,20 +14,22 @@ using UisSubsea.RovTopside.Logic;
 
 namespace UisSubsea.RovTopside.Presentation
 {
-    public partial class CoPilotView : Form, ICoPilotViewHandler
+    public partial class CoPilotView : Form, ICoPilotViewHandler, IView
     {
-        private Camera camera;
+        private ICamera camera;
         private Boolean fullScreen;
         private System.Drawing.SolidBrush Brush;
         //If there is water leak in controllbox
         private Boolean leak;
         //Used to change settings on pilotcam
 
-
-        public CoPilotView()
+        public CoPilotView(ICamera camera)
         {
             InitializeComponent();
-            SetDepth(300);            
+
+            this.camera = camera;
+            this.camera.Canvas = videoPictureBox;
+            this.camera.Start();
         }
 
         //Wil change color when there is a leak on the ROV. 
@@ -47,17 +49,11 @@ namespace UisSubsea.RovTopside.Presentation
             formGraphics.Dispose();
         }
 
-        private void CoPilotView_Load(object sender, EventArgs e)
-        {
-            camera = CameraFactory.GetManipulatorCamera();
-            camera.Canvas = videoPictureBox;
-            camera.Start();
-        }
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             if (camera != null)
             {
-                camera.Stop();
+                camera.Dispose();
             }
         }
 
@@ -78,41 +74,47 @@ namespace UisSubsea.RovTopside.Presentation
             }
 
         }
+
         //UI updates
         public void SetHeading(int heading)
         {
             headingGauge.Value = heading;
         }
+
         public void SetFrontCameraAngle(int angle)
-        { 
+        {
             frontCamGauge.Value = angle;
         }
+
         public void SetRearCameraAngle(int angle)
         {
             rearCamGauge.Value = angle;
         }
+
         public void SetDepth(double depth)
-        {                      
-          if(depth>600 && depth <= 1200)
+        {
+            if (depth > 600 && depth <= 1200)
             {
                 depthTrackBar1.Minimum = -1200;
             }
-          else if(depth>1200)
-          {
-              depthTrackBar1.Minimum = -3000;
-          }
-            depthTrackBar1.Value =(int)depth * -1;
+            else if (depth > 1200)
+            {
+                depthTrackBar1.Minimum = -3000;
+            }
+            depthTrackBar1.Value = (int)depth * -1;
             lblTextValueDepth.Text = depth.ToString();
             lblTextDepth.Text = "cm: ";
         }
+
         public void SetLaserDistanceMeasured(double distance)
         {
             lblTextDistance.Text = distance.ToString();
             if (distance > 0) { }//Add warning light when laser in use
         }
+
         public void setSensorState(bool sensorstate)
         {
-            if(sensorstate)
+            if (sensorstate)
             {
                 leak = false;
                 Invalidate();
@@ -124,18 +126,20 @@ namespace UisSubsea.RovTopside.Presentation
                 Invalidate();
             }
         }
+
         //Get called from CamController
-        public void setCamera(Camera camera)
+        public void SetCamera(ICamera camera)
         {
-            this.camera.Dispose();
+            this.camera.Stop();
             this.camera = camera;
             this.camera.Canvas = videoPictureBox;
             this.camera.Start();
         }
+
         //getcamera
         private void button2_Click(object sender, EventArgs e)
         {
-            
+
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -143,6 +147,10 @@ namespace UisSubsea.RovTopside.Presentation
             //this.camera.displayCameraProperties(IntPtr.Zero);
         }
 
+        public ICamera GetCamera()
+        {
+            return this.camera;
+        }
     }
 }
 
