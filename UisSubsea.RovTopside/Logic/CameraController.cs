bb06e480f1da5f12134d1282b46f6e23333802a2
MortenW <Morten_w92@hotmail.com>
2014-02-26 16:23:43 +0100

    new CameraController.cs

---
+++ UisSubsea.RovTopside/Logic/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UisSubsea.RovTopside.Presentation;
+using UisSubsea.RovTopside.Data;
+
+namespace UisSubsea.RovTopside.Logic
+{
+    class CameraController
+    {
+        private Camera
+
+        public CameraController(PilotView pilotView, CoPilotView coPilotView, Camera mainCamera,
+            Camera manipulatorCamera, Camera rearCamera)
+        {
+
+        }
+    }
+}
