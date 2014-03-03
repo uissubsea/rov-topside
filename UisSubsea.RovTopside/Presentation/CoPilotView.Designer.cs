27b0f9ade8e31ecb55e40968c24f200bd892ea00
MortenW <Morten_w92@hotmail.com>
2014-03-03 15:08:51 +0100

    chagne size copilotview

--- UisSubsea.RovTopside/Presentation/CoPilotView.Designer.cs
+++ UisSubsea.RovTopside/Presentation/CoPilotView.Designer.cs
@@ -53,7 +53,7 @@
             this.frontCamGauge.BaseArcWidth = 1;
             this.frontCamGauge.Center = new System.Drawing.Point(100, 100);
             this.frontCamGauge.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.frontCamGauge.Location = new System.Drawing.Point(1716, -1);
+            this.frontCamGauge.Location = new System.Drawing.Point(1055, 30);
             this.frontCamGauge.MaxValue = 105F;
             this.frontCamGauge.MinValue = -75F;
             this.frontCamGauge.Name = "frontCamGauge";
@@ -93,7 +93,7 @@
             this.button1.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.button1.ForeColor = System.Drawing.Color.Red;
-            this.button1.Location = new System.Drawing.Point(1776, 752);
+            this.button1.Location = new System.Drawing.Point(1055, 522);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 6;
@@ -109,7 +109,7 @@
             this.rearCamGauge.BaseArcSweep = 180;
             this.rearCamGauge.BaseArcWidth = 1;
             this.rearCamGauge.Center = new System.Drawing.Point(100, 100);
-            this.rearCamGauge.Location = new System.Drawing.Point(1722, 173);
+            this.rearCamGauge.Location = new System.Drawing.Point(1055, 211);
             this.rearCamGauge.MaxValue = 30F;
             this.rearCamGauge.MinValue = -150F;
             this.rearCamGauge.Name = "rearCamGauge";
@@ -148,7 +148,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Yellow;
-            this.label2.Location = new System.Drawing.Point(1793, 281);
+            this.label2.Location = new System.Drawing.Point(1126, 328);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(58, 12);
             this.label2.TabIndex = 15;
@@ -159,7 +159,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Yellow;
-            this.label3.Location = new System.Drawing.Point(1791, 109);
+            this.label3.Location = new System.Drawing.Point(1124, 140);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(60, 12);
             this.label3.TabIndex = 16;
@@ -169,7 +169,7 @@
             // 
             this.headingLabelText.AutoSize = true;
             this.headingLabelText.ForeColor = System.Drawing.Color.Yellow;
-            this.headingLabelText.Location = new System.Drawing.Point(1813, 527);
+            this.headingLabelText.Location = new System.Drawing.Point(1157, 532);
             this.headingLabelText.Name = "headingLabelText";
             this.headingLabelText.Size = new System.Drawing.Size(13, 13);
             this.headingLabelText.TabIndex = 17;
@@ -177,7 +177,7 @@
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(1825, 752);
+            this.trackBar1.Location = new System.Drawing.Point(1066, 571);
             this.trackBar1.Maximum = 1000;
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
@@ -188,9 +188,9 @@
             // 
             // videoPictureBox
             // 
-            this.videoPictureBox.Location = new System.Drawing.Point(12, -20);
+            this.videoPictureBox.Location = new System.Drawing.Point(-6, -31);
             this.videoPictureBox.Name = "videoPictureBox";
-            this.videoPictureBox.Size = new System.Drawing.Size(1920, 1080);
+            this.videoPictureBox.Size = new System.Drawing.Size(1055, 706);
             this.videoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.videoPictureBox.TabIndex = 0;
             this.videoPictureBox.TabStop = false;
@@ -198,17 +198,17 @@
             // headingIndicatorInstrumentControl1
             // 
             this.headingIndicatorInstrumentControl1.ForeColor = System.Drawing.Color.Yellow;
-            this.headingIndicatorInstrumentControl1.Location = new System.Drawing.Point(1745, 382);
+            this.headingIndicatorInstrumentControl1.Location = new System.Drawing.Point(1095, 410);
             this.headingIndicatorInstrumentControl1.Name = "headingIndicatorInstrumentControl1";
-            this.headingIndicatorInstrumentControl1.Size = new System.Drawing.Size(147, 142);
+            this.headingIndicatorInstrumentControl1.Size = new System.Drawing.Size(115, 119);
             this.headingIndicatorInstrumentControl1.TabIndex = 19;
             this.headingIndicatorInstrumentControl1.Text = "headingIndicatorInstrumentControl1";
             // 
             // altimeterInstrumentControl1
             // 
-            this.altimeterInstrumentControl1.Location = new System.Drawing.Point(1722, 554);
+            this.altimeterInstrumentControl1.Location = new System.Drawing.Point(1108, 557);
             this.altimeterInstrumentControl1.Name = "altimeterInstrumentControl1";
-            this.altimeterInstrumentControl1.Size = new System.Drawing.Size(179, 181);
+            this.altimeterInstrumentControl1.Size = new System.Drawing.Size(102, 102);
             this.altimeterInstrumentControl1.TabIndex = 18;
             this.altimeterInstrumentControl1.Text = "altimeterInstrumentControl1";
             // 
@@ -217,7 +217,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.WindowText;
-            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.headingIndicatorInstrumentControl1);
             this.Controls.Add(this.altimeterInstrumentControl1);
